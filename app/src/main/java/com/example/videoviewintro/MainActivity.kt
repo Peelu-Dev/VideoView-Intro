13d60c11a42699c81399fe45d2fa05360077a699
peelu123 <raghuwanshipalash6@gmail.com>
2022-06-28 16:14:02 +0530

    Added Increment logic to app

--- app/src/main/java/com/example/videoviewintro/MainActivity.kt
+++ app/src/main/java/com/example/videoviewintro/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.videoviewintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btnCount)
+        val textView = findViewById<TextView>(R.id.tvCount)
+
+        textView.text = count.toString()
+        btn.setOnClickListener{
+            ++count
+            textView.text = count.toString()
+
+        }
     }
+
 }
