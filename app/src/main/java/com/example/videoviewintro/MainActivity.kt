8f83a7c803895558be97d544063206cd5d311390
peelu123 <raghuwanshipalash6@gmail.com>
2022-06-28 16:25:37 +0530

    Added ViewModel to work app on auto-rotate mode also

--- app/src/main/java/com/example/videoviewintro/MainActivity.kt
+++ app/src/main/java/com/example/videoviewintro/MainActivity.kt
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     var count = 0
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         val btn = findViewById<Button>(R.id.btnCount)
         val textView = findViewById<TextView>(R.id.tvCount)
 
-        textView.text = count.toString()
+//        textView.text = count.toString()
+        textView.text = viewModel.count.toString()
         btn.setOnClickListener{
-            ++count
-            textView.text = count.toString()
+//            ++count
+            viewModel.updateCount()
+            textView.text = viewModel.count.toString()
 
         }
     }
---
+++ app/src/main/java/com/example/videoviewintro/MainActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.videoviewintro
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+    var count = 0
+
+    fun updateCount(){
+        ++count
+    }
+}
